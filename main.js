299b8c780f9983e0804f8ea2e5eb8a687dfd3add
ginohmk <michotall95@gmail.com>
2022-01-04 15:32:32 +0100

    Add navigation and javascript function

--- main.js
+++ main.js
@@ -3,59 +3,6 @@ const book = document.getElementById('book__container');
 const singleBook = document.createElement('div');
 singleBook.classList.add('book');
 const formOne = document.getElementById('form__one');
-<<<<<<< HEAD
-const addButton = document.getElementById('add__button');
-const inputBookTitle = document.getElementById('input__title');
-const inputAuthor = document.getElementById('input__author');
-const myLocalStorage = localStorage.getItem('data');
-
-// Array Stored book
-let storedBook = [];
-
-// Array of Objects
-const bookCollection = [];
-
-// Use array.push({})
-
-// REMOVE BOOK ####
-function removeBook(b) {
-  // Each Book item
-  const bookCollectionItem = Array.from(
-    document.getElementsByClassName('book')
-  );
-
-  Object.keys(bookCollectionItem).forEach((k) => {
-    if (b === k) {
-      bookCollectionItem[k].style.display = 'none';
-    }
-  });
-}
-
-// Create a function to ADD BOOK ####
-function addBook() {
-  // Save user input to Array Object
-  if (inputAuthor.value !== '' && inputBookTitle.value !== '') {
-    bookCollection.push({
-      title: inputBookTitle.value,
-      author: inputAuthor.value,
-    });
-
-    singleBook.innerHTML = `<p id="book__title">${inputBookTitle.value}</p>
-    <p id="book__aurthor">${inputAuthor.value}</p>
-    <button type="button" class="remove__button">Remove</button>
-    <hr />`;
-    book.appendChild(singleBook.cloneNode(true));
-    bookItems.appendChild(book);
-  }
-
-  // Define the remove button after Creation
-  const removeButton = Array.from(
-    document.getElementsByClassName('remove__button')
-  );
-  Object.keys(removeButton).forEach((removeKey) => {
-    removeButton[removeKey].addEventListener('click', () => {
-      removeBook(removeKey);
-=======
 const inputBookTitle = document.getElementById('input__title');
 const inputAuthor = document.getElementById('input__author');
 
@@ -113,27 +60,10 @@ function render() {
     const btn = removeButton[removeKey];
     btn.addEventListener('click', () => {
       BookInventory.removeBook(btn.parentNode);
->>>>>>> b95453399aea803049197a803aa8fe734e577593
     });
   }, false);
 }
 
-<<<<<<< HEAD
-// Get user data stored
-
-// Add eventListner
-
-addButton.addEventListener('click', addBook);
-
-// demo Local Storage
-
-// formOne.addEventListener('keyup', () => {
-//   getUserData();
-// });
-// if (myLocalStorage) {
-//   setUserData();
-// }
-=======
 // To Render the saved file on the Page
 render();
 
@@ -143,4 +73,3 @@ formOne.addEventListener('submit', (e) => {
   BookInventory.addBook();
   render();
 });
->>>>>>> b95453399aea803049197a803aa8fe734e577593
