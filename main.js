ca3d9646119f21d753534c73fac4df375df3261e
rachidelaid <rachidelaid@gmail.com>
2022-01-04 11:42:21 +0100

    moving the book list to the class constructor

--- main.js
+++ main.js
@@ -6,42 +6,44 @@ const formOne = document.getElementById('form__one');
 const inputBookTitle = document.getElementById('input__title');
 const inputAuthor = document.getElementById('input__author');
 
-// Array of Objects
-let bookCollection = localStorage.getItem('bookCollection')
-  ? JSON.parse(localStorage.getItem('bookCollection'))
-  : [];
-
 // USE CLASSES
-class BookInventory {
-  static addBook() {
-    // Save user input to Array Object
+class bookClass {
+  constructor() {
+    this.list = localStorage.getItem('bookCollection')
+      ? JSON.parse(localStorage.getItem('bookCollection'))
+      : [];
+  }
 
-    bookCollection.push({
+  addBook() {
+    // Save user input to Array Object
+    this.list.push({
       title: inputBookTitle.value,
       author: inputAuthor.value,
     });
-    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    localStorage.setItem('bookCollection', JSON.stringify(this.list));
 
     // Refactoring
     inputBookTitle.value = '';
     inputAuthor.value = '';
   }
 
-  static removeBook(b) {
+  removeBook(b) {
     // Parent Node with an ID of Book__title
     const title = b.querySelector('#book__title').innerText;
     b.remove();
-    bookCollection = bookCollection.filter((book) => book.title !== title);
+    this.list = this.list.filter((book) => book.title !== title);
 
-    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    localStorage.setItem('bookCollection', JSON.stringify(this.list));
   }
 }
+
+const BookInventory = new bookClass();
 
 // Render the LIST OF BOOKS
 function render() {
   book.innerHTML = '';
 
-  bookCollection.forEach((currentBook) => {
+  BookInventory.list.forEach((currentBook) => {
     singleBook.innerHTML = `
     <p id="book__aurthor">"<span id="book__title">${currentBook.title}</span>" ${currentBook.author}</p>
     <button type="button" class="remove__button">Remove</button>`;
